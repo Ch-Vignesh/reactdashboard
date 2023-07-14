bb26062a163c690e64d2ced5f624ced7370de925
Vignesh-ch <vigneshchinthakuntla4666@gmail.com>
2023-07-14 15:46:24 +0530

    new react app

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    style: {
+        postcss: {
+            pluggins: [
+                require('tailwindcss'),
+                require('autoprefixer'),
+                
+            ],
+        },
+    },
+};
